2288362ab6bb938e1edf08445bd202b0952a3165
Eric Kristensen <eric.l.kristensen@gmail.com>
2013-08-06 12:11:36 -0700

    add post for dynamic responses

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,17 @@
 get '/' do
-  @grandma = params[:grandma]
+  @grandma = params[:user_input]
   # Look in app/views/index.erb
   erb :index
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  user_input = params[:user_input]
+  if user_input == user_input.upcase
+    @grandma = "That's right, my mashed potatoes are the best!"
+  else
+    @grandma = "Speak up, kiddo!!"
+  end
+  erb :index
+  # redirect "/?grandma=#{user_input}"
+  # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
 end
